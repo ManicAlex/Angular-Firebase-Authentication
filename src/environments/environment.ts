4bfeb872d90769ca34f2560a444ba64032175d14
ManicAlex <45663871+ManicAlex@users.noreply.github.com>
2020-01-15 20:37:29 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,14 @@ export const environment = {
   production: false,
   
   firebase: {
-    apiKey:             "AIzaSyDEBztHyaGSDg_8RLkZOx-5oWAN9HOg_Ek",
-    authDomain:         "dx-auth-assessment.firebaseapp.com",
-    databaseURL:        "https://dx-auth-assessment.firebaseio.com",
-    projectId:          "dx-auth-assessment",
-    storageBucket:      "dx-auth-assessment.appspot.com",
-    messagingSenderId:  "903452123888",
-    appId:              "1:903452123888:web:d54993464eb4acbd635835",
-    measurementId:      "G-3P5FX018L6"
+    apiKey:             "###############",
+    authDomain:         "###############",
+    databaseURL:        "###############m",
+    projectId:          "###############",
+    storageBucket:      "###############",
+    messagingSenderId:  "###############",
+    appId:              "###############",
+    measurementId:      "###############"
   }
 };
 
